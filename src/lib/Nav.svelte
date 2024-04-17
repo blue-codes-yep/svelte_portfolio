14d2548193fe2680700fb3cb55ffe18e31cebcd2
blue-codes-yep <linzblue5@gmail.com>
2024-04-17 17:53:09 -0400

    Styling changes to the navbar, to make it look better on mobile.

--- src/lib/Nav.svelte
+++ src/lib/Nav.svelte
@@ -5,35 +5,46 @@
 <nav class="navbar">
   <a href="/" class="$page.path === '/' ? 'active' : ''">
     <div class="home-svg">
-    <svg
-      viewBox="0 0 24 24"
-      fill="none"
-      xmlns="http://www.w3.org/2000/svg"
-      style="stroke: var(--home-svg-stroke);"
-      width="30"
-      height="30"
-      ><g id="SVGRepo_bgCarrier" stroke-width="0" /><g
-        id="SVGRepo_tracerCarrier"
-        stroke-linecap="round"
-        stroke-linejoin="round"
-      /><g id="SVGRepo_iconCarrier">
-        <path
-          d="M2.36407 12.9579C1.98463 10.3208 1.79491 9.00229 2.33537 7.87495C2.87583 6.7476 4.02619 6.06234 6.32691 4.69181L7.71175 3.86687C9.80104 2.62229 10.8457 2 12 2C13.1543 2 14.199 2.62229 16.2882 3.86687L17.6731 4.69181C19.9738 6.06234 21.1242 6.7476 21.6646 7.87495C22.2051 9.00229 22.0154 10.3208 21.6359 12.9579L21.3572 14.8952C20.8697 18.2827 20.626 19.9764 19.451 20.9882C18.2759 22 16.5526 22 13.1061 22H10.8939C7.44737 22 5.72409 22 4.54903 20.9882C3.37396 19.9764 3.13025 18.2827 2.64284 14.8952L2.36407 12.9579Z"
-          style="stroke: var(--home-svg-stroke);"
-          stroke-width="1.5"
-        />
-        <path
-          d="M15 18H9"
-          style="stroke: var(--home-svg-stroke);"
-          stroke-width="1.5"
+      <svg
+        viewBox="0 0 24 24"
+        fill="none"
+        xmlns="http://www.w3.org/2000/svg"
+        style="stroke: var(--home-svg-stroke);"
+        width="30"
+        height="30"
+        ><g id="SVGRepo_bgCarrier" stroke-width="0" /><g
+          id="SVGRepo_tracerCarrier"
           stroke-linecap="round"
-        />
-      </g></svg
-    ></div>
+          stroke-linejoin="round"
+        /><g id="SVGRepo_iconCarrier">
+          <path
+            d="M2.36407 12.9579C1.98463 10.3208 1.79491 9.00229 2.33537 7.87495C2.87583 6.7476 4.02619 6.06234 6.32691 4.69181L7.71175 3.86687C9.80104 2.62229 10.8457 2 12 2C13.1543 2 14.199 2.62229 16.2882 3.86687L17.6731 4.69181C19.9738 6.06234 21.1242 6.7476 21.6646 7.87495C22.2051 9.00229 22.0154 10.3208 21.6359 12.9579L21.3572 14.8952C20.8697 18.2827 20.626 19.9764 19.451 20.9882C18.2759 22 16.5526 22 13.1061 22H10.8939C7.44737 22 5.72409 22 4.54903 20.9882C3.37396 19.9764 3.13025 18.2827 2.64284 14.8952L2.36407 12.9579Z"
+            style="stroke: var(--home-svg-stroke);"
+            stroke-width="1.5"
+          />
+          <path
+            d="M15 18H9"
+            style="stroke: var(--home-svg-stroke);"
+            stroke-width="1.5"
+            stroke-linecap="round"
+          />
+        </g></svg
+      >
+    </div>
   </a>
-  <div class="nav-about"><a href="#about" class="$page.path === '/#about' ? 'active' : ''">About</a></div>
-  <div class="nav-projects"><a href="#project" class="$page.path === '/#project' ? 'active' : ''">Projects</a></div>
-  <div class="nav-contact"><a href="#contact" class="$page.path === '/#contact' ? 'active' : ''">Contact</a></div>
+  <div class="nav-about">
+    <a href="#about" class="$page.path === '/#about' ? 'active' : ''">About</a>
+  </div>
+  <div class="nav-projects">
+    <a href="#project" class="$page.path === '/#project' ? 'active' : ''"
+      >Projects</a
+    >
+  </div>
+  <div class="nav-contact">
+    <a href="#contact" class="$page.path === '/#contact' ? 'active' : ''"
+      >Contact</a
+    >
+  </div>
   <ThemeToggle />
 </nav>
 
@@ -45,6 +56,15 @@
     "home about projects contact toggle";
 }
 
+@media (max-width: 428px) {
+  .navbar {
+  display: grid;
+  grid-template-columns: 10px 200px 200px;
+  grid-template-areas: 
+    "home about projects contact toggle";
+}
+}
+
 .home-svg {
   position: fixed;
   grid-area: home;
@@ -54,6 +74,7 @@
 
 .nav-about {
   grid-area: about;
+  padding-left: 30px;
 }
 
 .nav-projects {
